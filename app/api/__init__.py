1464e546ca3ffb820f930fb2b0667ee20286a7e8
wjdguswn1203 <wjdguswn0954@gmail.com>
2024-07-11 01:04:48 +0000

    Revert: remove file & folder

--- app/api/__init__.py
+++
@@ -1,5 +0,0 @@
-# app/api/v1/__init__.py
-from .paper import endpoints as paper_endpoints
-from .trans import endpoints as trans_endpoints
-from .ocr import endpoints as ocr_endpoints
-from .keyword import endpoints as keyword_endpoints
