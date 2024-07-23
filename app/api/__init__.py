18fde2dc2c27af1cab1020cbd6e74c4c4263b77a
wjdguswn1203 <wjdguswn0954@gmail.com>
2024-07-23 08:49:12 +0000

    Refactor: summary api만 남기기

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,2 @@
 # app/api/v1/__init__.py
 from .summary import endpoints as summary_endpoints
-from .topic import endpoints as topic_endpoints
-from .weaviate import endpoints as weaviate_endpoints
-from .translate import endpoints as translate_endpoints
